3e4423586309b8e758455e3204863b5d0a51b6a8
Adrien Cransac <adrien.cransac@gmail.com>
2020-04-07 22:42:07 +0200

    test_processes: Redefined parseEnvironmentTree test with new test helper checkEnvironmentTreeFirstEntry that controls the first entry and its possible first child entry                 Renamed test_parseEnvironmentTree to test_parseEnvironmentTreeWithObject

--- test_processes.js
+++ test_processes.js
@@ -7,52 +7,58 @@ const { continuation, floatOn, forget, later, now, value } = require('streamer')
 const Test = require('tester');
 const { inputIsCapture, skipToDebuggerPausedAfterStepping, userInput } = require('./testutils.js');
 
-function test_parseEnvironmentTree(finish, check) {
-  const testSession = (send, render, terminate) => {
-    const controlSelection = message => refreshSelectedEnvironmentTree(makeSelectionInEnvironmentTree(makeEnvironmentTree()),
-	                                                               readEnvironmentTree(message));
+function checkEnvironmentTreeFirstEntry(entryDescription, firstChildEntryDescription) {
+  return (finish, check) => {
+    return (send, render, terminate) => {
+      const controlSelection = message => refreshSelectedEnvironmentTree(makeSelectionInEnvironmentTree(makeEnvironmentTree()),
+                                                                         readEnvironmentTree(message));
 
-    const firstCheck = async (stream) => {
-      if (isDebuggerPaused(message(stream))) {
-        return firstCheck(await continuation(now(stream))(forget(await later(stream))));
-      }
-      else if (isEnvironmentTree(message(stream))
-	         && (selection => selectedEntryName(selectedEntry(selection)) === "/Object test")
-	              (controlSelection(message(stream)))) {
-	userInput("l");
+      const firstCheck = async (stream) => {
+        if (isDebuggerPaused(message(stream))) {
+          return firstCheck(await continuation(now(stream))(forget(await later(stream))));
+        }
+        else if (isEnvironmentTree(message(stream))
+                   && (selection => selectedEntryName(selectedEntry(selection)) === `/${entryDescription}`)
+                        (controlSelection(message(stream)))) {
+          userInput("l");
 
-        return secondCheck(await continuation(now(stream))(forget(await later(stream))));
-      }
-      else {
-        return floatOn(stream, false);
-      }
-    };
+          return secondCheck(await continuation(now(stream))(forget(await later(stream))));
+        }
+        else {
+          return floatOn(stream, false);
+        }
+      };
 
-    const secondCheck = async (stream) => {
-      if (isEnvironmentTreeFocus(message(stream))) {
-        return secondCheck(await continuation(now(stream))(forget(await later(stream))));
-      }
-      else if (isEnvironmentTree(message(stream))
-	         && (selection => selectedEntryName(selectedEntry(selection)) === "/Object test/String a: \"abc\"")
-	              (visitChildEntry(controlSelection(message(stream))))) {
-        return floatOn(stream, true);
-      }
-      else {
-        return floatOn(stream, false);
-      }
-    };
+      const secondCheck = async (stream) => {
+        if (isEnvironmentTreeFocus(message(stream))) {
+          return secondCheck(await continuation(now(stream))(forget(await later(stream))));
+        }
+        else if (isEnvironmentTree(message(stream))
+                   && (selection => selectedEntryName(selectedEntry(selection))
+          		            === `/${entryDescription}${firstChildEntryDescription ? "/" + firstChildEntryDescription
+          		                                                                  : ""}`)
+                        (visitChildEntry(controlSelection(message(stream))))) {
+          return floatOn(stream, true);
+        }
+        else {
+          return floatOn(stream, false);
+        }
+      };
 
-    return async (stream) => {
-      return finish(terminate(check(value(now(await firstCheck
-	                                       (await parseEnvironmentTree(send)
-	                                         (await inputIsCapture(makeEnvironmentTreeFocus)
-					           (await skipToDebuggerPausedAfterStepping(send, 1)(stream)))))))));
+      return async (stream) => {
+        return finish(terminate(check(value(now(await firstCheck
+                                                 (await parseEnvironmentTree(send)
+                                                   (await inputIsCapture(makeEnvironmentTreeFocus)
+          				           (await skipToDebuggerPausedAfterStepping(send, 1)(stream)))))))));
+      };
     };
   };
+}
 
-  init(["node", "app.js", "test_target.js"], testSession);
+function test_parseEnvironmentTreeWithObject(finish, check) {
+  init(["node", "app.js", "test_target.js"], checkEnvironmentTreeFirstEntry("Object test", "String a: \"abc\"")(finish, check));
 }
 
 Test.run([
-  Test.makeTest(test_parseEnvironmentTree, "Parse Environment Tree")
+  Test.makeTest(test_parseEnvironmentTreeWithObject, "Parse Environment Tree With Object")
 ]);
