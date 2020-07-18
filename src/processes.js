f014861d473c7331a5c1f8d9ff1a9cd1f938f425
Adrien Cransac <adrien.cransac@gmail.com>
2020-07-18 15:30:21 +0200

    documentation: Reordered and documented functions in processes.js

--- src/processes.js
+++ src/processes.js
@@ -1,9 +1,42 @@
 const { insertInEnvironmentTree, makeEnvironmentTree, makePendingEntriesRegister, makeSelectionInEnvironmentTree, refreshSelectedEnvironmentTree, resolvePendingEntry } = require('./environmenttree.js');
-const { branches, insertInFileTree, makeFileEntry, makeFileTree, parseFilePath } = require('filetree');
+const { insertInFileTree, makeFileEntry, makeFileTree, parseFilePath } = require('filetree');
 const { ctrlCInput, displayedScriptSource, enterInput, exploreEnvironmentTree, parseUserInput } = require('./helpers.js');
-const { breakpointCapture, breakpointLine, endCapture, hasEnded, input, interactionKeys, isBreakpointCapture, isDebuggerPaused, isEnvironment, isEnvironmentEntry, isInput, isQueryCapture, isScriptParsed, isUserScriptParsed, makeBreakpointCapture, makeEnvironmentTreeFocus, makeEnvironmentTreeMessage, makeError, makeMessagesFocus, makeQueryCapture, makeSourceTreeFocus, makeSourceTreeMessage, message, parsedScriptHandle, parsedScriptUrl, parsedUserScriptPath, query, readEnvironment, sendContinue, sendQuery, sendRequestForEnvironmentDescription, sendRequestForScriptSource, sendSetBreakpoint, sendStepInto, sendStepOut, sendStepOver } = require('./protocol.js');
+const { breakpointCapture, breakpointLine, endCapture, hasEnded, input, interactionKeys, isBreakpointCapture, isDebuggerPaused, isEnvironment, isEnvironmentEntry, isInput, isQueryCapture, isUserScriptParsed, makeBreakpointCapture, makeEnvironmentTreeFocus, makeEnvironmentTreeMessage, makeError, makeMessagesFocus, makeQueryCapture, makeSourceTreeFocus, makeSourceTreeMessage, message, parsedScriptHandle, parsedUserScriptPath, query, readEnvironment, sendContinue, sendQuery, sendRequestForEnvironmentDescription, sendRequestForScriptSource, sendSetBreakpoint, sendStepInto, sendStepOut, sendStepOver } = require('./protocol.js');
 const { commit, continuation, floatOn, forget, now, later } = require('streamer');
 
+/*
+ * Record a breakpoint
+ * @param {function} send - The callback to send requests over websocket to Inspector
+ * @return {Process}
+ */
+function addBreakpoint(send) {
+  const breakpointSetter = scriptId => breakpointLine => {
+    sendSetBreakpoint(send, scriptId, breakpointLine);
+  };
+
+  const breakpointAdder = (setBreakpoint, displayChange) => async (stream) => {
+    const updateBreakpointSetter = (displayChange, scriptId) => {
+      return commit(stream, breakpointAdder(breakpointSetter(scriptId), displayChange));
+    };
+
+    if (isBreakpointCapture(message(stream)) && hasEnded(message(stream))) {
+      setBreakpoint(breakpointLine(message(stream)));
+
+      return commit(stream, breakpointAdder(setBreakpoint, displayChange));
+    }
+    else {
+      return displayChange(updateBreakpointSetter, updateBreakpointSetter)(stream);
+    }
+  };
+
+  return breakpointAdder(breakpointSetter(undefined), displayedScriptSource());
+}
+
+/*
+ * Start and end captures and focuses, and mark one input character as a capture or focus when they are active
+ * @param {Stream} stream - The stream
+ * @return {Stream} - The process outputs capture and focus messages with one character on user input messages 
+ */
 async function changeMode(stream) {
   const modalCapture = (makeCapture, continuation) => {
     const modeSetter = async (stream) => {
@@ -48,6 +81,35 @@ async function changeMode(stream) {
   }
 }
 
+/*
+ * Trigger the stream's continuation until the user terminates the session. Also, on exception, revert the complete process to the last successful pass and input an error message with the exception details
+ * @param {function} terminate - The terminate function handed over by Terminal
+ * @return {Process}
+ */
+function loop(terminate) {
+  const looper = async (stream) => {
+    if (isInput(message(stream)) && input(message(stream)) === ctrlCInput()) {
+      return terminate();
+    }
+    else {
+      const afterwards = await later(stream);
+
+      try {
+        return looper(await continuation(now(stream))(forget(afterwards)));
+      }
+      catch (error) {
+        return looper(await continuation(now(stream))(forget(await floatOn(afterwards, makeError(error.stack)))));
+      }
+    }
+  };
+
+  return looper;
+}
+
+/*
+ * Concatenate or delete user inputs in capture mode
+ * @return {Process} - The process outputs capture messages with concatenated inputs on single-character capture messages
+ */
 function parseCaptures() {
   const parser = capture => async (stream) => {
     const parse = (makeCapture, readCapture) => {
@@ -75,25 +137,11 @@ function parseCaptures() {
   return parser("");
 }
 
-function parseSourceTree() {
-  const builder = sourceTree => async (stream) => {
-    if (isUserScriptParsed(message(stream))) {
-      const [path, fileName] = parseFilePath(parsedUserScriptPath(message(stream)));
-
-      const newSourceTree = insertInFileTree(sourceTree,
-	                                     path,
-	                                     makeFileEntry(fileName, parsedScriptHandle(message(stream))));
-
-      return floatOn(commit(stream, builder(newSourceTree)), makeSourceTreeMessage(newSourceTree));
-    }
-    else {
-      return commit(stream, builder(sourceTree));
-    }
-  };
-
-  return builder(makeFileTree());
-}
-
+/*
+ * Make and update the environment tree, and query for the environment and its deferred entries
+ * @param {function} send - The callback to send requests over websocket to Inspector
+ * @return {Process} - The process outputs environment tree messages on environment and environment entry messages
+ */
 function parseEnvironmentTree(send) {
   const builder = (environmentTree, selection, pendingEntriesRegister) => async (stream) => {
     if (isDebuggerPaused(message(stream))) {
@@ -133,6 +181,34 @@ function parseEnvironmentTree(send) {
   return builder(makeEnvironmentTree(), makeSelectionInEnvironmentTree(makeEnvironmentTree()), makePendingEntriesRegister());
 }
 
+/*
+ * Make and update the source tree
+ * @return {Process} - The process outputs source tree messages on user script parsed messages
+ */
+function parseSourceTree() {
+  const builder = sourceTree => async (stream) => {
+    if (isUserScriptParsed(message(stream))) {
+      const [path, fileName] = parseFilePath(parsedUserScriptPath(message(stream)));
+
+      const newSourceTree = insertInFileTree(sourceTree,
+	                                     path,
+	                                     makeFileEntry(fileName, parsedScriptHandle(message(stream))));
+
+      return floatOn(commit(stream, builder(newSourceTree)), makeSourceTreeMessage(newSourceTree));
+    }
+    else {
+      return commit(stream, builder(sourceTree));
+    }
+  };
+
+  return builder(makeFileTree());
+}
+
+/*
+ * Query the displayed script source when it changes
+ * @param {function} send - The callback to send requests over websocket to Inspector
+ * @return {Process}
+ */
 function pullScriptSource(send) {
   const scriptChecker = displayChange => async (stream) => {
     const onDisplayChange = (displayChange, newDisplayScriptId) => {
@@ -151,6 +227,11 @@ function pullScriptSource(send) {
   return scriptChecker(displayedScriptSource());
 }
 
+/*
+ * Send user defined queries to Inspector
+ * @param {function} send - The callback to send requests over websocket to Inspector
+ * @return {Process}
+ */
 function queryInspector(send) {
   const requester = async (stream) => {
     if (isQueryCapture(message(stream)) && hasEnded(message(stream))) {
@@ -166,6 +247,11 @@ function queryInspector(send) {
   return requester;
 }
 
+/*
+ * Step through the execution
+ * @param {function} send - The callback to send requests over websocket to Inspector
+ * @return {Process}
+ */
 function step(send) {
   const stepper = async (stream) => {
     if (isInput(message(stream)) && input(message(stream)) === interactionKeys("stepOver")) {
@@ -185,49 +271,6 @@ function step(send) {
   };
 
   return stepper;
-}
-
-function addBreakpoint(send) {
-  const breakpointSetter = scriptId => breakpointLine => {
-    sendSetBreakpoint(send, scriptId, breakpointLine);
-  };
-
-  const breakpointAdder = (setBreakpoint, displayChange) => async (stream) => {
-    const updateBreakpointSetter = (displayChange, scriptId) => {
-      return commit(stream, breakpointAdder(breakpointSetter(scriptId), displayChange));
-    };
-
-    if (isBreakpointCapture(message(stream)) && hasEnded(message(stream))) {
-      setBreakpoint(breakpointLine(message(stream)));
-
-      return commit(stream, breakpointAdder(setBreakpoint, displayChange));
-    }
-    else {
-      return displayChange(updateBreakpointSetter, updateBreakpointSetter)(stream);
-    }
-  };
-
-  return breakpointAdder(breakpointSetter(undefined), displayedScriptSource());
-}
-
-function loop(terminate) {
-  const looper = async (stream) => {
-    if (isInput(message(stream)) && input(message(stream)) === ctrlCInput()) {
-      return terminate();
-    }
-    else {
-      const afterwards = await later(stream);
-
-      try {
-        return looper(await continuation(now(stream))(forget(afterwards)));
-      }
-      catch (error) {
-        return looper(await continuation(now(stream))(forget(await floatOn(afterwards, makeError(error.stack)))));
-      }
-    }
-  };
-
-  return looper;
 }
 
 module.exports = {
