98af38baa84dfb792e85a975b33a76dde6559835
Adrien Cransac <adrien.cransac@gmail.com>
2020-07-22 20:53:52 +0200

    documentation: Reordered and documented functions in testutils.js

--- src/testutils.js
+++ src/testutils.js
@@ -1,6 +1,14 @@
 const { input, isDebuggerPaused, isInput, makeInput, message, sendStepOver } = require('./protocol.js');
 const { commit, floatOn, later } = require('streamer');
 
+// # Initialize Tests
+
+/*
+ * Skip all messages until the debugger is paused after some steps have been registered in the program's execution
+ * @param {function} send - The callback to send requests over websocket to Inspector
+ * @param {number} stepsToMake - The number of steps to register after the execution has started
+ * @return {Process} - The process to start with in the composition passed to the test's streamer downstream
+ */
 function skipToDebuggerPausedAfterStepping(send, stepsToMake) {
   const skipper = stepsToMake => {
     return async (stream) => {
@@ -21,7 +29,15 @@ function skipToDebuggerPausedAfterStepping(send, stepsToMake) {
   return skipper(stepsToMake);
 }
 
-// Input sequence type --
+// # Mock User Input
+// ## Input Sequence
+
+/*
+ * Make an input sequence
+ * @param {string[]} inputs - An array of characters which are the mocked user inputs, occuring in the array's order
+ * @param {number} [inputsPerSecond: 1] - The number of inputs to trigger per second
+ * @return {InputSequence}
+ */
 function makeInputSequence(inputs, inputsPerSecond) {
   return [inputs, inputsPerSecond ? inputsPerSecond : 1];
 }
@@ -34,6 +50,41 @@ function inputsPerSecond(inputSequence) {
   return inputSequence[1];
 }
 
+// ## Record Inputs
+
+/*
+ * Assign each user input to a capture
+ * @param {function} makeCapture - The capture's constructor
+ * @return {Process} - The process outputs the given capture associated with the key on input messages
+ */
+function inputIsCapture(makeCapture) {
+  const captureMaker = async (stream) => {
+    if (isInput(message(stream))) {
+      return commit(floatOn(stream, makeCapture(input(message(stream)))), captureMaker);
+    }
+    else {
+      return commit(stream, captureMaker);
+    }
+  };
+
+  return captureMaker;
+}
+
+/*
+ * Create an array of characters with the same key repeated
+ * @param {string} key - The character to repeat
+ * @param {number} count - The number of times to repeat the key
+ * @return {string[]}
+ */
+function repeatKey(key, count) {
+  return new Array(count).fill(key);
+}
+
+/*
+ * Trigger the standard input with predefined keys
+ * @param {...InputSequence} inputSequences - A series of input sequences to trigger in turn, at their specified rate
+ * @return {}
+ */
 function userInput(...inputSequences) {
   const registerInput = (delay, inputSequences) => {
     if (inputSequences.length === 0) {
@@ -50,23 +101,6 @@ function userInput(...inputSequences) {
   };
 
   return registerInput(0, inputSequences);
-}
-
-function inputIsCapture(makeCapture) {
-  const captureMaker = async (stream) => {
-    if (isInput(message(stream))) {
-      return commit(floatOn(stream, makeCapture(input(message(stream)))), captureMaker);
-    }
-    else {
-      return commit(stream, captureMaker);
-    }
-  };
-
-  return captureMaker;
-}
-
-function repeatKey(key, count) {
-  return new Array(count).fill(key);
 }
 
 module.exports = {
