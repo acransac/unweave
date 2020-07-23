71bbda6328eefe57c8ee2d56497cc005fe8a5264
Adrien Cransac <adrien.cransac@gmail.com>
2020-07-23 23:47:31 +0200

    Linting

--- test/test_environmenttree.js
+++ test/test_environmenttree.js
@@ -35,9 +35,9 @@ function makeFakeEnvironmentEntriesFromInspector(values) {
           type: "object",
           subtype: typeCategory,
           className: valueTypeName,
-	  description: `${valueTypeName}(${lengthFunction(value)})`,
-	  objectId: makeFakeRemoteHandle(id)
-	};
+          description: `${valueTypeName}(${lengthFunction(value)})`,
+          objectId: makeFakeRemoteHandle(id)
+        };
       };
     };
 
@@ -64,9 +64,9 @@ function makeFakeEnvironmentEntriesFromInspector(values) {
     else if (typeof value === "function") {
       return {
         type: "function",
-	className: `${value.toString().startsWith("async") ? "Async" : ""}Function`,
-	description: value.toString(),
-	objectId: makeFakeRemoteHandle(id)
+        className: `${value.toString().startsWith("async") ? "Async" : ""}Function`,
+        description: value.toString(),
+        objectId: makeFakeRemoteHandle(id)
       };
     }
     // value can only be an object in the following cases
@@ -75,43 +75,43 @@ function makeFakeEnvironmentEntriesFromInspector(values) {
     }
     else if (instanceOf([Array], value).length > 0) {
       return (prototypeName => enumerableValueFromInspector("array", value => value.length)
-	                         (prototypeName, value))
+                                 (prototypeName, value))
         (instanceOf([Array], value)[0].name);
     }
     else if (instanceOf([DataView], value).length > 0) {
       return (prototypeName => enumerableValueFromInspector("dataview", value => value.byteLength)
-	                         (prototypeName, value))
+                                 (prototypeName, value))
         (instanceOf([DataView], value)[0].name);
     }
     else if (instanceOf([Map, Set], value).length > 0) {
       return (prototypeName => enumerableValueFromInspector(prototypeName.toLowerCase(), value => value.size)
-	                         (prototypeName, value))
+                                 (prototypeName, value))
         (instanceOf([Map, Set], value)[0].name);
     }
     else if (instanceOf(typedArrayPrototypes, value).length > 0) {
       return enumerableValueFromInspector("typedarray", value => value.length)
-	       (instanceOf(typedArrayPrototypes, value)[0].name, value);
+               (instanceOf(typedArrayPrototypes, value)[0].name, value);
     }
     else if (instanceOf([ArrayBuffer, SharedArrayBuffer], value).length > 0) {
       return enumerableValueFromInspector("arraybuffer", value => value.byteLength)
-	       (instanceOf([ArrayBuffer, SharedArrayBuffer], value)[0].name, value);
+               (instanceOf([ArrayBuffer, SharedArrayBuffer], value)[0].name, value);
     }
     else if (value instanceof Promise) {
       return {
         type: "object",
-	subtype: "promise",
-	className: "Promise",
-	description: "Promise",
-	objectId: makeFakeRemoteHandle(id)
+        subtype: "promise",
+        className: "Promise",
+        description: "Promise",
+        objectId: makeFakeRemoteHandle(id)
       };
     }
     else if (util.types.isProxy(value)) {
       return {
         type: "object",
-	subtype: "proxy",
-	className: "Object",
-	description: "Proxy",
-	objectId: makeFakeRemoteHandle(id)
+        subtype: "proxy",
+        className: "Object",
+        description: "Proxy",
+        objectId: makeFakeRemoteHandle(id)
       };
     }
     // General object
@@ -130,120 +130,120 @@ function test_emptyEnvironmentTree(finish, check) {
   const emptyEnvironmentTree = makeEnvironmentTree();
 
   return finish(check(root(emptyEnvironmentTree) === "/env"
-	                && branches(emptyEnvironmentTree).length === 0));
+                        && branches(emptyEnvironmentTree).length === 0));
 }
 
 function test_selectionInEmptyEnvironmentTree(finish, check) {
   const emptySelection = makeSelectionInEnvironmentTree(makeEnvironmentTree());
 
   return finish(check(selectedBranch(emptySelection).length === 0
-	                && selectedEntryName(selectedEntry(emptySelection)) === ""
-	                && selectedEntryLeafName(selectedEntry(emptySelection)) === ""
-	                && selectedEntryBranchName(selectedEntry(emptySelection)) === ""));
+                        && selectedEntryName(selectedEntry(emptySelection)) === ""
+                        && selectedEntryLeafName(selectedEntry(emptySelection)) === ""
+                        && selectedEntryBranchName(selectedEntry(emptySelection)) === ""));
 }
 
 function test_environmentTreeWithOneImmediateEntry(finish, check) {
   const [environmentTree, selection] = makeEnvironment([["/env", makeFakeEnvironmentEntriesFromInspector(["abc"])]]);
 
   return finish(check(selectedBranch(selection).length === 1
-	                && selectedEntryName(selectedEntry(selection)) === "/String entry0: \"abc\""
-	                && selectedEntryLeafName(selectedEntry(selection)) === "String entry0: \"abc\""
-	                && selectedEntryBranchName(selectedEntry(selection)) === ""
-	                && !isVisitableEntrySelected(selectedEntry(selection))
-	                && !isDeferredEntrySelected(selectedEntry(selection))));
+                        && selectedEntryName(selectedEntry(selection)) === "/String entry0: \"abc\""
+                        && selectedEntryLeafName(selectedEntry(selection)) === "String entry0: \"abc\""
+                        && selectedEntryBranchName(selectedEntry(selection)) === ""
+                        && !isVisitableEntrySelected(selectedEntry(selection))
+                        && !isDeferredEntrySelected(selectedEntry(selection))));
 }
 
 function test_environmentTreeWithOneDeferredEntry(finish, check) {
   const [environmentTree, selection] = makeEnvironment([["/env", makeFakeEnvironmentEntriesFromInspector([{}])]]);
 
   return finish(check(selectedBranch(selection).length === 1
-	                && selectedEntryName(selectedEntry(selection)) === "/Object entry0"
-	                && selectedEntryLeafName(selectedEntry(selection)) === "Object entry0"
-	                && selectedEntryBranchName(selectedEntry(selection)) === ""
-	                && isVisitableEntrySelected(selectedEntry(selection))
-	                && !isDeferredEntrySelected(selectedEntry(selection))
-	                && (deferredEntry => selectedEntryName(deferredEntry) === "/Object entry0/deferred"
-			                       && selectedEntryLeafName(deferredEntry) === "deferred"
-			                       && selectedEntryBranchName(deferredEntry) === "/Object entry0"
-	                                       && !isVisitableEntrySelected(deferredEntry)
-	                                       && isDeferredEntrySelected(deferredEntry))
-			     (selectedEntry(visitChildEntry(selection)))));
+                        && selectedEntryName(selectedEntry(selection)) === "/Object entry0"
+                        && selectedEntryLeafName(selectedEntry(selection)) === "Object entry0"
+                        && selectedEntryBranchName(selectedEntry(selection)) === ""
+                        && isVisitableEntrySelected(selectedEntry(selection))
+                        && !isDeferredEntrySelected(selectedEntry(selection))
+                        && (deferredEntry => selectedEntryName(deferredEntry) === "/Object entry0/deferred"
+                                               && selectedEntryLeafName(deferredEntry) === "deferred"
+                                               && selectedEntryBranchName(deferredEntry) === "/Object entry0"
+                                               && !isVisitableEntrySelected(deferredEntry)
+                                               && isDeferredEntrySelected(deferredEntry))
+                             (selectedEntry(visitChildEntry(selection)))));
 }
 
 function test_environmentTreeExploration(finish, check) {
   const [environmentTree, selection] = makeEnvironment([["/env", makeFakeEnvironmentEntriesFromInspector([{}, "abc"])]]);
 
   const isEmptyObject = entry => selectedEntryName(entry) === "/Object entry0"
-	                           && selectedEntryLeafName(entry) === "Object entry0"
-	                           && selectedEntryBranchName(entry) === ""
-	                           && isVisitableEntrySelected(entry)
-	                           && !isDeferredEntrySelected(entry);
+                                   && selectedEntryLeafName(entry) === "Object entry0"
+                                   && selectedEntryBranchName(entry) === ""
+                                   && isVisitableEntrySelected(entry)
+                                   && !isDeferredEntrySelected(entry);
 
   return finish(check(isEmptyObject(selectedEntry(selection))
-	                && (stringEntry => selectedEntryName(stringEntry) === "/String entry1: \"abc\""
-			                     && selectedEntryLeafName(stringEntry) === "String entry1: \"abc\""
-			                     && selectedEntryBranchName(stringEntry) === ""
-	                                     && !isVisitableEntrySelected(stringEntry)
-	                                     && !isDeferredEntrySelected(stringEntry))
-	                     (selectedEntry(selectNextEntry(selection)))
-	                && isEmptyObject(selectedEntry(selectPreviousEntry(selectNextEntry(selection))))
-	                && (deferredEntry => selectedEntryName(deferredEntry) === "/Object entry0/deferred"
-			                       && selectedEntryLeafName(deferredEntry) === "deferred"
-			                       && selectedEntryBranchName(deferredEntry) === "/Object entry0"
-	                                       && !isVisitableEntrySelected(deferredEntry)
-	                                       && isDeferredEntrySelected(deferredEntry))
-			     (selectedEntry(visitChildEntry(selection)))
-	                && isEmptyObject(selectedEntry(visitParentEntry(visitChildEntry(selection))))));
+                        && (stringEntry => selectedEntryName(stringEntry) === "/String entry1: \"abc\""
+                                             && selectedEntryLeafName(stringEntry) === "String entry1: \"abc\""
+                                             && selectedEntryBranchName(stringEntry) === ""
+                                             && !isVisitableEntrySelected(stringEntry)
+                                             && !isDeferredEntrySelected(stringEntry))
+                             (selectedEntry(selectNextEntry(selection)))
+                        && isEmptyObject(selectedEntry(selectPreviousEntry(selectNextEntry(selection))))
+                        && (deferredEntry => selectedEntryName(deferredEntry) === "/Object entry0/deferred"
+                                               && selectedEntryLeafName(deferredEntry) === "deferred"
+                                               && selectedEntryBranchName(deferredEntry) === "/Object entry0"
+                                               && !isVisitableEntrySelected(deferredEntry)
+                                               && isDeferredEntrySelected(deferredEntry))
+                             (selectedEntry(visitChildEntry(selection)))
+                        && isEmptyObject(selectedEntry(visitParentEntry(visitChildEntry(selection))))));
 }
 
 function test_resolveDeferredEntry(finish, check) {
   const testSession = (send, render, terminate) => {
     const queryDeferredEntry = () => {
       const requester = (environmentTree, selection, pendingEntriesRegister) => async (stream) => {
-	if (isDebuggerPaused(message(stream))) {
-	  return requester(environmentTree, selection, pendingEntriesRegister)
-		   (await later(await parseEnvironmentTree(send)(stream)));
+        if (isDebuggerPaused(message(stream))) {
+          return requester(environmentTree, selection, pendingEntriesRegister)
+                   (await later(await parseEnvironmentTree(send)(stream)));
         }
-	else if (isEnvironment(message(stream))) {
+        else if (isEnvironment(message(stream))) {
           const [newEnvironmentTree, newSelection] = makeEnvironment([["/env", readEnvironment(message(stream))]], send);
 
-	  const deferredSelection = visitChildEntry(newSelection);
+          const deferredSelection = visitChildEntry(newSelection);
 
-	  return requester(newEnvironmentTree,
-		           deferredSelection,
-		           registerPendingEntry(pendingEntriesRegister, deferredSelection))
-		   (await later(stream));
+          return requester(newEnvironmentTree,
+                           deferredSelection,
+                           registerPendingEntry(pendingEntriesRegister, deferredSelection))
+                   (await later(stream));
         }
-	else if (isEnvironmentEntry(message(stream))) {
-	  const finishTest = (environmentTree, selection, pendingEntriesRegister) => {
-	    return floatOn(stream, selectedEntryName(selectedEntry(selection)) === "/Object test/String a: \"abc\""
-	                             && selectedEntryLeafName(selectedEntry(selection)) === "String a: \"abc\""
-	                             && selectedEntryBranchName(selectedEntry(selection)) === "/Object test"
-	                             && !isVisitableEntrySelected(selectedEntry(selection))
-	                             && !isDeferredEntrySelected(selectedEntry(selection)));
-	  };
+        else if (isEnvironmentEntry(message(stream))) {
+          const finishTest = (environmentTree, selection, pendingEntriesRegister) => {
+            return floatOn(stream, selectedEntryName(selectedEntry(selection)) === "/Object test/String a: \"abc\""
+                                     && selectedEntryLeafName(selectedEntry(selection)) === "String a: \"abc\""
+                                     && selectedEntryBranchName(selectedEntry(selection)) === "/Object test"
+                                     && !isVisitableEntrySelected(selectedEntry(selection))
+                                     && !isDeferredEntrySelected(selectedEntry(selection)));
+          };
 
           return resolvePendingEntry(environmentTree,
-		                     selection,
-		                     pendingEntriesRegister,
-		                     message(stream),
-		                     entries => entries.filter(entry => entry.isOwn),
-		                     send,
-		                     finishTest);
+                                     selection,
+                                     pendingEntriesRegister,
+                                     message(stream),
+                                     entries => entries.filter(entry => entry.isOwn),
+                                     send,
+                                     finishTest);
         }
-	else {
-	  return floatOn(stream, false);
+        else {
+          return floatOn(stream, false);
         }
       };
 
       return requester(makeEnvironmentTree(),
-	               makeSelectionInEnvironmentTree(makeEnvironmentTree()),
-	               makePendingEntriesRegister());
+                       makeSelectionInEnvironmentTree(makeEnvironmentTree()),
+                       makePendingEntriesRegister());
     };
 
     return async (stream) => {
       return terminate(check(value(now(await queryDeferredEntry()
-	                                (await skipToDebuggerPausedAfterStepping(send, 1)(stream))))));
+                                        (await skipToDebuggerPausedAfterStepping(send, 1)(stream))))));
     };
   };
 
@@ -258,14 +258,14 @@ function test_explorationSkipsDeferredEntries(finish, check) {
   ]);
 
   const isExpectedObject = entry => selectedEntryName(entry) === "/Object entry0/Object entry0"
-	                              && selectedEntryLeafName(entry) === "Object entry0"
-	                              && selectedEntryBranchName(entry) === "/Object entry0"
-	                              && isVisitableEntrySelected(entry)
-	                              && !isDeferredEntrySelected(entry);
+                                      && selectedEntryLeafName(entry) === "Object entry0"
+                                      && selectedEntryBranchName(entry) === "/Object entry0"
+                                      && isVisitableEntrySelected(entry)
+                                      && !isDeferredEntrySelected(entry);
 
   return finish(check(isExpectedObject(selectedEntry(visitChildEntry(selection)))
-	                && isExpectedObject(selectedEntry(selectPreviousEntry(visitChildEntry(selection))))
-	                && isExpectedObject(selectedEntry(visitParentEntry(visitChildEntry(visitChildEntry(selection)))))));
+                        && isExpectedObject(selectedEntry(selectPreviousEntry(visitChildEntry(selection))))
+                        && isExpectedObject(selectedEntry(visitParentEntry(visitChildEntry(visitChildEntry(selection)))))));
 }
 
 module.exports = Test.run([
